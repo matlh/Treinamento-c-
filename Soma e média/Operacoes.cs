8be929b66ed77e4b94a5280315809f77fa85f19c
Matheus Lima Hilário <matheuslimahilario@gmail.com>
2023-07-17 15:48:02 -0300

    Exercício de soma e média

---
+++ Soma e média/Operacoes.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Soma_e_média
+{
+    public class Operacoes
+    {
+        
+    }
+}
---
+++ Soma e média/SomaEMedia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SomaEMedia
+{
+    public class SomaEMedia
+    {
+        public static void Main()
+        {
+            
+            decimal[] Numeros = new decimal[9]; 
+
+            System.Console.WriteLine ("Digite a qunatidade de valores para cálculo de média e soma: ");
+            byte n = byte.Parse(System.Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                System.Console.WriteLine ($"Digite o {i+1}° número: ");
+                Numeros [i] = decimal.Parse(Console.ReadLine());
+            }
+
+        }
+
+    }
+}
