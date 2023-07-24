58bbd0d9fcb61e5040d6d079059d60f8a548f2b7
Matheus Lima Hilário <matheuslimahilario@gmail.com>
2023-07-24 20:12:54 -0300

    Entrada de jogadores e iniclização de objetos

--- Jogo de dados/Jogador.cs
+++ Jogo de dados/Jogador.cs
@@ -7,6 +7,17 @@ namespace Jogo_de_dados
 {
     public class Jogador
     {
-        
+       public string Nome;
+       public int Pontuacao = 0;
+
+       public Jogador (string Nome)
+       {
+            this.Nome = Nome;
+       }
+       
+       public void Ponto()
+       {
+            Pontuacao++;
+       }
     }
 }
--- Jogo de dados/Jogo.cs
+++ Jogo de dados/Jogo.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Jogo_de_dados;
 
 namespace Jogo
 {
@@ -6,7 +7,18 @@ namespace Jogo
     {
         public static void Main()
         {
-            
+            Console.WriteLine("Digite o nome do primeiro jogador");
+            Jogador J1 = new Jogador(Console.ReadLine());
+
+            Console.WriteLine("Digite o nome do segundo jogador");
+            Jogador J2 = new Jogador(Console.ReadLine());
+
+            J1.Ponto();
+            J1.Ponto();
+            J1.Ponto();
+            J1.Ponto();
+
+            Console.WriteLine($"Nome do jogador 1 é: {J1.Nome}\n Pontuação do Jogador 1 é: {J1.Pontuacao}");
         }
     }
 }
